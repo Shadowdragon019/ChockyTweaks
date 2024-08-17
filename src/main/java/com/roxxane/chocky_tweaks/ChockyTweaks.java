ae081291b0a8bcb429fcc2dfa631bd973ac7da70
Sammy [any/all] <Shadowdragon019019@gmail.com>
2024-08-17 10:24:39 -0500

    Initial commit

---
+++ src/main/java/com/roxxane/chocky_tweaks/ChockyTweaks.java
@@ -0,0 +1,13 @@
+package com.roxxane.chocky_tweaks;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(ChockyTweaks.modId)
+public class ChockyTweaks {
+    public static final String modId = "chocky_tweaks";
+    private static final Logger logger = LogUtils.getLogger();
+
+    public ChockyTweaks() {}
+}
